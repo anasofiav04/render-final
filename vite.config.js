20aed2870f4f8954a3997fce1028284844db485a
anasofiav04 <anasofiav04@gmail.com>
2025-12-03 08:29:30 -0600

    Subir proyecto completo sin dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./render-final/", // esto es clave para GitHub Pages
+  base: "/render-final/", // esto es clave para GitHub Pages
 });
